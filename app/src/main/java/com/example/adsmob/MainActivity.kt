196b42fa79780d02bf57b12ac04f83b0e1bdaefd
Barira Ushshaque <barira.ushshaque@gmail.com>
2021-01-17 22:41:20 +0530

    Version 1.0

--- app/src/main/java/com/example/adsmob/MainActivity.kt
+++ app/src/main/java/com/example/adsmob/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.adsmob
-import android.R
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
+package com.example.adsmob;
 
+//import android.R
+import android.os.Bundle
+//import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
